30499e6958adbdb1780b509f82fbb13bcce4734c
KIM CHEOL JUN <cjfwns966@gmail.com>
2022-03-24 05:38:00 +0900

    writepage memozing

--- blog_project/blog-frontend/src/componenets/write/TagBox.js
+++ blog_project/blog-frontend/src/componenets/write/TagBox.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo } from "react";
 
 import {
   TagBoxBlock,
@@ -6,25 +6,29 @@ import {
   TagList,
   TagItem,
 } from "./styled-component/styledTagBox";
-// TagList memo
-const Tag = React.memo(({ tag, onRemoveTag }) => {
+// Tag memoized
+const Tag = ({ tag, onRemoveTag }) => {
   console.log("Tag Render");
   return <TagItem onClick={onRemoveTag}>{tag}</TagItem>;
-});
+};
 
-// React.memo를 해줌으로써 input이 바뀌어도 Tags 컴포넌트는 리렌더링되지않는다.
-const Tags = React.memo(({ tags, onRemoveTag }) => {
+const MemoizedTag = memo(Tag);
+
+// memo를 해줌으로써 input이 바뀌어도 Tags 컴포넌트는 리렌더링되지않는다.
+// Tags memoized
+const Tags = ({ tags, onRemoveTag }) => {
   console.log("Tags Render");
   return (
     <TagList>
       {tags.map((tag, index) => (
-        <Tag key={index} tag={tag} onRemoveTag={onRemoveTag} />
+        <MemoizedTag key={index} tag={tag} onRemoveTag={onRemoveTag} />
       ))}
     </TagList>
   );
-});
+};
 
-// TagItem memo
+const MemoizedTags = memo(Tags);
+
 const TagBox = ({ tagText, tags, tagTextChange, onAddTag, onRemoveTag }) => {
   return (
     <TagBoxBlock>
@@ -37,7 +41,7 @@ const TagBox = ({ tagText, tags, tagTextChange, onAddTag, onRemoveTag }) => {
         />
         <button>추가</button>
       </TagForm>
-      <Tags tags={tags} onRemoveTag={onRemoveTag} />
+      <MemoizedTags tags={tags} onRemoveTag={onRemoveTag} />
     </TagBoxBlock>
   );
 };
